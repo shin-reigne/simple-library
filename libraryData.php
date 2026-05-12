8b1ad4b08bc044fec84c27480c275a753fd39e46
Shinosaurus <shinreignedelrosario@gmail.com>
2026-05-12 23:41:50 +0800

    datas

---
+++ libraryData.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'Book.php';
+//this are the books created 
+$library = array(
+    new Book("Noli Me Tangere","Jose Rizal","",0),
+    new Book("Dekada 70","Lualhati Bautista","Jericho",6),
+    new Book("Smaller and Smaller Circles","F.H Batacan","Angel",8),
+    new Book("War Drove","Troy Cabida","Michelle",16)
+);
+?>
